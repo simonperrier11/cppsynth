71b6c3723c1835d11d5075c31db8aa7848a902d8
Simon Perrier <simonperrier11@hotmail.com>
2024-01-16 15:22:22 -0500

    Added notes in PluginProcessor header file

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -6,28 +6,37 @@
   ==============================================================================
 */
 
+// LRN Skip current header if already included once (non-standard)
 #pragma once
 
 #include <JuceHeader.h>
 
 //==============================================================================
 /**
+ LRN The CppsynthAudioProcessor class extends the juce::AudioProcessor, which contains all audio processing classes
+ and functions.
 */
 class CppsynthAudioProcessor  : public juce::AudioProcessor
 {
 public:
     //==============================================================================
+    // LRN Constructor
     CppsynthAudioProcessor();
     ~CppsynthAudioProcessor() override;
 
     //==============================================================================
+    // LRN Tells plug-in about sample rate, buffer size, allocates resources
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    
+    // LRN Releases resources
     void releaseResources() override;
 
+    // LRN #conditionals are preprocessor directives (before compilation)
    #ifndef JucePlugin_PreferredChannelConfigurations
     bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
    #endif
 
+    // LRN All audio processing is done here
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
     //==============================================================================
@@ -35,6 +44,7 @@ public:
     bool hasEditor() const override;
 
     //==============================================================================
+    // LRN Info that will be queried by the host
     const juce::String getName() const override;
 
     bool acceptsMidi() const override;
@@ -55,5 +65,8 @@ public:
 
 private:
     //==============================================================================
+    // LRN Disable copy constructor (constructor that takes reference to other object of
+    // same class) so the class cannot be copied
+    // LRN Also enables JUCE built-in memory leak detector
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CppsynthAudioProcessor)
 };
