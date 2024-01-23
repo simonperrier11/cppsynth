a10e82acd9161546b9a0636a2499b7789cd1db5d
Simon Perrier <simonperrier11@hotmail.com>
2024-01-23 15:17:40 -0500

    Added Constants header file

---
+++ Source/Constants.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    Constants.h
+    Created: 23 Jan 2024 2:30:14pm
+    Author:  Simon Perrier
+
+  ==============================================================================
+*/
+
+#pragma once
+
+/**
+ This namespace contains constant values used throughout the plugin. Of course, re-compilation is needed when
+ changing any of these.
+ */
+namespace constants
+{
+    // The value used for non-playing notes
+    inline constexpr int noNoteValue { -1 };
+}
