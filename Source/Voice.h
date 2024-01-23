e8c65a9ab4bbbb602ba480d072c9029c32caec9b
Simon Perrier <simonperrier11@hotmail.com>
2024-01-23 15:24:26 -0500

    Added note and velocity members to Voice, added reset() function to Voice

--- Source/Voice.h
+++ Source/Voice.h
@@ -9,3 +9,26 @@
 */
 
 #pragma once
+#include "Constants.h"
+
+// LRN we can use a struct instead of a class when we don't need private/protected
+//  and other inheritance shenanigans (class defaults to private, struct to public)
+/**
+ Represents a voice for the synthesizer; produces the next output sample for a given note.
+ The synthesizer can have multiple voices in polyphony.
+ */
+struct Voice
+{
+    int note;
+    int velocity;
+    
+    /**
+     Resets the state of the voice instance.
+     */
+    void reset()
+    {
+        note = constants::noNoteValue;
+        velocity = 0;
+    }
+};
+
