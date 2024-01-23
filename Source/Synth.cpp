a5375c4b0b206fd8f847c9d16dbdd91be2898896
Simon Perrier <simonperrier11@hotmail.com>
2024-01-23 15:25:45 -0500

    Added basic functions for Synth : resources allocation, resetting, MIDI messages handling for note on and note off

--- Source/Synth.cpp
+++ Source/Synth.cpp
@@ -9,3 +9,76 @@
 */
 
 #include "Synth.h"
+
+Synth::Synth()
+{
+    // Default sample rate to 44.1Hz
+    sampleRate = 44100.0f;
+}
+
+// LRN trailing _ here used to distinguish with private member sampleRate
+void Synth::allocateResources(double sampleRate_, int /*samplesPerBlock*/) {
+    // LRN static_cast has more compile-time checks than regular cast, and is safer
+    sampleRate = static_cast<float>(sampleRate_);
+}
+
+void Synth::deallocateResources()
+{
+    // TODO
+}
+
+void Synth::reset()
+{
+    voice.reset();
+}
+
+void Synth::render(float** outputBuffers, int sampleCount)
+{
+    // TOOD
+}
+
+void Synth::midiMessage(uint8_t data0, uint8_t data1, uint8_t data2)
+{
+    // The status byte (data0) has 2 parts: command (first 4 bits) and
+    // channel (last 4 bits). We simply need to do a binary AND with 11110000
+    // and 00001111 to get both parts respectively
+    uint8_t command = data0 & 0xF0;
+    uint8_t channel = data0 & 0x0F;
+    
+    // Force set values to 0-127 range by doing binary AND, just in case
+    uint8_t note = data1 & 0x7F;
+    uint8_t velocity = data2 & 0x7F;
+
+    switch (command) {
+        case 0x80: { // Note Off command code
+            noteOff(note, velocity);
+            break;
+        }
+        case 0x90: { // Note On command code
+            if (velocity > 0) {
+                noteOn(note, velocity);
+            }
+            else {
+                // Note On with no velocity is treated as Note Off
+                // (running status optimization)
+                noteOff(note, velocity);
+            }
+            break;
+        }
+    }
+}
+
+void Synth::noteOn(int note, int velocity)
+{
+    voice.note = note;
+    voice.velocity = velocity;
+}
+
+void Synth::noteOff(int note, int velocity)
+{
+    // TODO if noteOff velocity is to be implemented, use here
+    if (voice.note == note) {
+        voice.note = constants::noNoteValue;
+        voice.velocity = 0;
+    }
+}
--- Source/Synth.h
+++ Source/Synth.h
@@ -9,3 +9,57 @@
 */
 
 #pragma once
+
+// LRN include <file.h> for std lib stuff or stuff in predefined directory,
+//  "file.h" for programmer-defined stuff
+#include <JuceHeader.h>
+#include "Constants.h"
+#include "Voice.h"
+
+/**
+ Represents the synthesizer as a whole. The synthesizer handles MIDI, renders audio.
+ */
+class Synth
+{
+public:
+    Synth();
+    
+    /**
+     Allocates memory for audio rendering.
+     */
+    void allocateResources(double sampleRate, int samplesPerBlock);
+    
+    /**
+     Deallocate memory after usage finished.
+     */
+    void deallocateResources();
+    
+    /**
+     Resets the state of the Synth instance.
+     */
+    void reset();
+    
+    /**
+     Renders audio in outputBuffers.
+     */
+    void render(float** outputBuffers, int sampleCount);
+    
+    /**
+     TODO
+     */
+    void midiMessage(uint8_t data0, uint8_t data1, uint8_t data2);
+    
+private:
+    float sampleRate;
+    Voice voice;
+    
+    /**
+     Handles the Note On command.
+     */
+    void noteOn(int note, int velocity);
+    
+    /**
+     Handles the Note Off command.
+     */
+    void noteOff(int note, int velocity);
+};
