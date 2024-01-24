b8a6b6cadbadba77720a4f17b87d60591d7b5cb5
Simon Perrier <simonperrier11@hotmail.com>
2024-01-24 10:21:48 -0500

    Added header file for SineOscillator class

---
+++ Source/SineOscillator.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    SineOscillator.h
+    Created: 24 Jan 2024 10:14:21am
+    Author:  Simon Perrier
+
+  ==============================================================================
+*/
+
+#pragma once
+
+// 2 * pi
+const float TWO_PI = 6.2831853071795864f;
+
+/**
+ Represents a basic oscillator.
+ */
+class SineOscillator
+{
+    
+}
