1a512a3cb18490f12034525e342c5022af381326
Simon Perrier <simonperrier11@hotmail.com>
2024-04-05 14:20:55 -0400

    Added many UI elements

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,21 +15,98 @@ CppsynthAudioProcessorEditor::CppsynthAudioProcessorEditor (CppsynthAudioProcess
     // Set global look and feel
     juce::LookAndFeel::setDefaultLookAndFeel(&globalLNF);
     
-    // Add elements to UI
-    // Poly mode button 
-    polyModeButton.setButtonText(juce::CharPointer_UTF8("Poly"));
-    polyModeButton.setClickingTogglesState(true);
-    addAndMakeVisible(polyModeButton);
+    // OSC1
+    osc1LevelKnob.label = "Level";
+    addAndMakeVisible(osc1LevelKnob);
+    osc1MorphKnob.label = "Shape";
+    addAndMakeVisible(osc1MorphKnob);
+
+    // Noise
+    noiseTypeButton.setButtonText(juce::CharPointer_UTF8("Noise Type"));
+    noiseTypeButton.setClickingTogglesState(true);
+    addAndMakeVisible(noiseTypeButton);
+    noiseLevelKnob.label = "Level";
+    addAndMakeVisible(noiseLevelKnob);
     
-    // Output level knob
-    outputLevelKnob.label = "Output Level";
-    addAndMakeVisible(outputLevelKnob);
+    // OSC2
+    osc2LevelKnob.label = "Level";
+    addAndMakeVisible(osc2LevelKnob);
+    osc2MorphKnob.label = "Shape";
+    addAndMakeVisible(osc2MorphKnob);
+    oscTuneKnob.label = "Semitones";
+    addAndMakeVisible(oscTuneKnob);
+    oscFineKnob.label = "Cents";
+    addAndMakeVisible(oscFineKnob);
     
-    // LPF Q knob
-    lpfResoKnob.label = "LPF Q";
+    // ENV
+    envAttackKnob.label = "Attack";
+    addAndMakeVisible(envAttackKnob);
+    envDecayKnob.label = "Decay";
+    addAndMakeVisible(envDecayKnob);
+    envSustainKnob.label = "Sustain";
+    addAndMakeVisible(envSustainKnob);
+    envReleaseKnob.label = "Release";
+    addAndMakeVisible(envReleaseKnob);
+    
+    // LPF
+    lpfFreqKnob.label = "Frequency";
+    addAndMakeVisible(lpfFreqKnob);
+    lpfResoKnob.label = "Q";
     addAndMakeVisible(lpfResoKnob);
+    lpfEnvKnob.label = "Envelope %";
+    addAndMakeVisible(lpfEnvKnob);
+    lpfLFOKnob.label = "LFO %";
+    addAndMakeVisible(lpfLFOKnob);
     
-    setSize (600, 400);
+    // LPF ENV
+    lpfAttackKnob.label = "Attack";
+    addAndMakeVisible(lpfAttackKnob);
+    lpfDecayKnob.label = "Decay";
+    addAndMakeVisible(lpfDecayKnob);
+    lpfSustainKnob.label = "Sustain";
+    addAndMakeVisible(lpfSustainKnob);
+    lpfReleaseKnob.label = "Release";
+    addAndMakeVisible(lpfReleaseKnob);
+    
+    // HPF
+    hpfFreqKnob.label = "Frequency";
+    addAndMakeVisible(hpfFreqKnob);
+    hpfResoKnob.label = "Q";
+    addAndMakeVisible(hpfResoKnob);
+    hpfEnvKnob.label = "Envelope %";
+    addAndMakeVisible(hpfEnvKnob);
+    hpfLFOKnob.label = "LFO %";
+    addAndMakeVisible(hpfLFOKnob);
+
+    // HPF ENV
+    hpfAttackKnob.label = "Attack";
+    addAndMakeVisible(hpfAttackKnob);
+    hpfDecayKnob.label = "Decay";
+    addAndMakeVisible(hpfDecayKnob);
+    hpfSustainKnob.label = "Sustain";
+    addAndMakeVisible(hpfSustainKnob);
+    hpfReleaseKnob.label = "Release";
+    addAndMakeVisible(hpfReleaseKnob);
+    
+    // LFO
+    lfoRateKnob.label = "LFO Rate";
+    addAndMakeVisible(lfoRateKnob);
+    vibratoKnob.label = "Depth";
+    addAndMakeVisible(vibratoKnob);
+
+
+
+//    polyModeButton.setButtonText(juce::CharPointer_UTF8("Poly"));
+//    polyModeButton.setClickingTogglesState(true);
+//    addAndMakeVisible(polyModeButton);
+//    outputLevelKnob.label = "Output Level";
+//    addAndMakeVisible(outputLevelKnob);
+    
+//    // Plugin size on open
+//    setResizable(true, false);
+//    // LRN: minX, minY, maxX, maxY
+//    setResizeLimits(600, 400, 1400, 1000);
+    setSize(1100, 500);
 }
 
 CppsynthAudioProcessorEditor::~CppsynthAudioProcessorEditor()
@@ -49,11 +126,97 @@ void CppsynthAudioProcessorEditor::paint(juce::Graphics& g)
 void CppsynthAudioProcessorEditor::resized()
 {
     // Rectangle bound for UI elements
-    juce::Rectangle r(20, 20, 100, 120);
+    // LRN: params are posX, posY, sizeX, sizeY
+    juce::Rectangle osc1Elem(20, 20, 80, 100);
+    juce::Rectangle osc2Elem(140, 20, 80, 100);
+    juce::Rectangle envElem(260, 20, 80, 100);
+    juce::Rectangle lfoElem(380, 20, 80, 100);
+    juce::Rectangle lpfElem(380 + 120, 20, 80, 100);
+    juce::Rectangle lpfEnvElem(465 + 120, 20, 80, 100);
+    juce::Rectangle hpfElem(575 + 120, 20, 80, 100);
+    juce::Rectangle hpfEnvElem(660 + 120, 20, 80, 100);
     
-    outputLevelKnob.setBounds(r);
-    r = r.withX(r.getRight() + 20);
-    lpfResoKnob.setBounds(r);
-    polyModeButton.setSize(80, 30);
-    polyModeButton.setCentrePosition(r.withX(r.getRight()).getCentre());
+    // OSC1
+    osc1LevelKnob.setBounds(osc1Elem);
+    osc1Elem = osc1Elem.withY(osc1Elem.getBottom() + 20);
+    osc1MorphKnob.setBounds(osc1Elem);
+    osc1Elem = osc1Elem.withY(osc1Elem.getBottom() + 20);
+    
+    // Noise
+    noiseTypeButton.setBounds(osc1Elem);
+    osc1Elem = osc1Elem.withY(osc1Elem.getBottom() + 20);
+    noiseLevelKnob.setBounds(osc1Elem);
+    osc1Elem = osc1Elem.withY(osc1Elem.getBottom() + 20);
+
+    // OSC2
+    osc2LevelKnob.setBounds(osc2Elem);
+    osc2Elem = osc2Elem.withY(osc2Elem.getBottom() + 20);
+    osc2MorphKnob.setBounds(osc2Elem);
+    osc2Elem = osc2Elem.withY(osc2Elem.getBottom() + 20);
+    oscTuneKnob.setBounds(osc2Elem);
+    osc2Elem = osc2Elem.withY(osc2Elem.getBottom() + 20);
+    oscFineKnob.setBounds(osc2Elem);
+    osc2Elem = osc2Elem.withY(osc2Elem.getBottom() + 20);
+    
+    // ENV
+    envAttackKnob.setBounds(envElem);
+    envElem = envElem.withY(envElem.getBottom() + 20);
+    envDecayKnob.setBounds(envElem);
+    envElem = envElem.withY(envElem.getBottom() + 20);
+    envSustainKnob.setBounds(envElem);
+    envElem = envElem.withY(envElem.getBottom() + 20);
+    envReleaseKnob.setBounds(envElem);
+    envElem = envElem.withY(envElem.getBottom() + 20);
+    
+    // OSC1
+    lfoRateKnob.setBounds(lfoElem);
+    lfoElem = lfoElem.withY(lfoElem.getBottom() + 20);
+    vibratoKnob.setBounds(lfoElem);
+    lfoElem = lfoElem.withY(lfoElem.getBottom() + 20);
+
+    // LPF
+    lpfFreqKnob.setBounds(lpfElem);
+    lpfElem = lpfElem.withY(lpfElem.getBottom() + 20);
+    lpfResoKnob.setBounds(lpfElem);
+    lpfElem = lpfElem.withY(lpfElem.getBottom() + 20);
+    lpfEnvKnob.setBounds(lpfElem);
+    lpfElem = lpfElem.withY(lpfElem.getBottom() + 20);
+    lpfLFOKnob.setBounds(lpfElem);
+    lpfElem = lpfElem.withY(lpfElem.getBottom() + 20);
+    
+    // LPF ENV
+    lpfAttackKnob.setBounds(lpfEnvElem);
+    lpfEnvElem = lpfEnvElem.withY(lpfEnvElem.getBottom() + 20);
+    lpfDecayKnob.setBounds(lpfEnvElem);
+    lpfEnvElem = lpfEnvElem.withY(lpfEnvElem.getBottom() + 20);
+    lpfSustainKnob.setBounds(lpfEnvElem);
+    lpfEnvElem = lpfEnvElem.withY(lpfEnvElem.getBottom() + 20);
+    lpfReleaseKnob.setBounds(lpfEnvElem);
+    lpfEnvElem = lpfEnvElem.withY(lpfEnvElem.getBottom() + 20);
+
+    // HPF
+    hpfFreqKnob.setBounds(hpfElem);
+    hpfElem = hpfElem.withY(hpfElem.getBottom() + 20);
+    hpfResoKnob.setBounds(hpfElem);
+    hpfElem = hpfElem.withY(hpfElem.getBottom() + 20);
+    hpfEnvKnob.setBounds(hpfElem);
+    hpfElem = hpfElem.withY(hpfElem.getBottom() + 20);
+    hpfLFOKnob.setBounds(hpfElem);
+    hpfElem = hpfElem.withY(hpfElem.getBottom() + 20);
+    
+    // HPF ENV
+    hpfAttackKnob.setBounds(hpfEnvElem);
+    hpfEnvElem = hpfEnvElem.withY(hpfEnvElem.getBottom() + 20);
+    hpfDecayKnob.setBounds(hpfEnvElem);
+    hpfEnvElem = hpfEnvElem.withY(hpfEnvElem.getBottom() + 20);
+    hpfSustainKnob.setBounds(hpfEnvElem);
+    hpfEnvElem = hpfEnvElem.withY(hpfEnvElem.getBottom() + 20);
+    hpfReleaseKnob.setBounds(hpfEnvElem);
+    hpfEnvElem = hpfEnvElem.withY(hpfEnvElem.getBottom() + 20);
+
+//    polyModeButton.setSize(80, 30);
+//    polyModeButton.setCentrePosition(r.withX(r.getRight()).getCentre());
+//    outputLevelKnob.setBounds(r);
+//    r = r.withX(r.getRight() + 20);
+//
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -49,11 +49,89 @@ private:
     RotaryKnob outputLevelKnob;
     SliderAttachment outputLevelAttachment { audioProcessor.apvts, ParameterID::outputLevel.getParamID(), outputLevelKnob.slider };
 
+    // OSC1
+    RotaryKnob osc1LevelKnob;
+    SliderAttachment osc1LevelAttachment { audioProcessor.apvts, ParameterID::osc1Level.getParamID(), osc1LevelKnob.slider };
+    RotaryKnob osc1MorphKnob;
+    SliderAttachment osc1MorphAttachment { audioProcessor.apvts, ParameterID::osc1Morph.getParamID(), osc1MorphKnob.slider };
+    
+    // Noise
+    RotaryKnob noiseLevelKnob;
+    SliderAttachment noiseLevelAttachment { audioProcessor.apvts, ParameterID::noiseLevel.getParamID(), noiseLevelKnob.slider };
+
+    // OSC2
+    RotaryKnob osc2LevelKnob;
+    SliderAttachment osc2LevelAttachment { audioProcessor.apvts, ParameterID::osc2Level.getParamID(), osc2LevelKnob.slider };
+    RotaryKnob osc2MorphKnob;
+    SliderAttachment osc2MorphAttachment { audioProcessor.apvts, ParameterID::osc2Morph.getParamID(), osc2MorphKnob.slider };
+    RotaryKnob oscTuneKnob;
+    SliderAttachment oscTuneAttachment { audioProcessor.apvts, ParameterID::oscTune.getParamID(), oscTuneKnob.slider };
+    RotaryKnob oscFineKnob;
+    SliderAttachment oscFineAttachment { audioProcessor.apvts, ParameterID::oscFine.getParamID(), oscFineKnob.slider };
+    
+    // ENV
+    RotaryKnob envAttackKnob;
+    SliderAttachment envAttackAttachment { audioProcessor.apvts, ParameterID::envAttack.getParamID(), envAttackKnob.slider };
+    RotaryKnob envDecayKnob;
+    SliderAttachment envDecayAttachment { audioProcessor.apvts, ParameterID::envDecay.getParamID(), envDecayKnob.slider };
+    RotaryKnob envSustainKnob;
+    SliderAttachment envSustainAttachment { audioProcessor.apvts, ParameterID::envSustain.getParamID(), envSustainKnob.slider };
+    RotaryKnob envReleaseKnob;
+    SliderAttachment envReleaseAttachment { audioProcessor.apvts, ParameterID::envRelease.getParamID(), envReleaseKnob.slider };
+
+    // LPF
+    RotaryKnob lpfFreqKnob;
+    SliderAttachment lpfFreqAttachment { audioProcessor.apvts, ParameterID::lpfFreq.getParamID(), lpfFreqKnob.slider };
     RotaryKnob lpfResoKnob;
     SliderAttachment lpfResoAttachment { audioProcessor.apvts, ParameterID::lpfReso.getParamID(), lpfResoKnob.slider };
+    RotaryKnob lpfEnvKnob;
+    SliderAttachment lpfEnvAttachment { audioProcessor.apvts, ParameterID::lpfEnv.getParamID(), lpfEnvKnob.slider };
+    RotaryKnob lpfLFOKnob;
+    SliderAttachment lpfLFOAttachment { audioProcessor.apvts, ParameterID::lpfLFO.getParamID(), lpfLFOKnob.slider };
     
+    // LPF ENV
+    RotaryKnob lpfAttackKnob;
+    SliderAttachment lpfAttackAttachment { audioProcessor.apvts, ParameterID::lpfAttack.getParamID(), lpfAttackKnob.slider };
+    RotaryKnob lpfDecayKnob;
+    SliderAttachment lpfDecayAttachment { audioProcessor.apvts, ParameterID::lpfDecay.getParamID(), lpfDecayKnob.slider };
+    RotaryKnob lpfSustainKnob;
+    SliderAttachment lpfSustainAttachment { audioProcessor.apvts, ParameterID::lpfSustain.getParamID(), lpfSustainKnob.slider };
+    RotaryKnob lpfReleaseKnob;
+    SliderAttachment lpfReleaseAttachment { audioProcessor.apvts, ParameterID::lpfRelease.getParamID(), lpfReleaseKnob.slider };
+    
+    // HPF
+    RotaryKnob hpfFreqKnob;
+    SliderAttachment hpfFreqAttachment { audioProcessor.apvts, ParameterID::hpfFreq.getParamID(), hpfFreqKnob.slider };
+    RotaryKnob hpfResoKnob;
+    SliderAttachment hpfResoAttachment { audioProcessor.apvts, ParameterID::hpfReso.getParamID(), hpfResoKnob.slider };
+    RotaryKnob hpfEnvKnob;
+    SliderAttachment hpfEnvAttachment { audioProcessor.apvts, ParameterID::hpfEnv.getParamID(), hpfEnvKnob.slider };
+    RotaryKnob hpfLFOKnob;
+    SliderAttachment hpfLFOAttachment { audioProcessor.apvts, ParameterID::hpfLFO.getParamID(), hpfLFOKnob.slider };
+
+    // HPF ENV
+    RotaryKnob hpfAttackKnob;
+    SliderAttachment hpfAttackAttachment { audioProcessor.apvts, ParameterID::hpfAttack.getParamID(), hpfAttackKnob.slider };
+    RotaryKnob hpfDecayKnob;
+    SliderAttachment hpfDecayAttachment { audioProcessor.apvts, ParameterID::hpfDecay.getParamID(), hpfDecayKnob.slider };
+    RotaryKnob hpfSustainKnob;
+    SliderAttachment hpfSustainAttachment { audioProcessor.apvts, ParameterID::hpfSustain.getParamID(), hpfSustainKnob.slider };
+    RotaryKnob hpfReleaseKnob;
+    SliderAttachment hpfReleaseAttachment { audioProcessor.apvts, ParameterID::hpfRelease.getParamID(), hpfReleaseKnob.slider };
+    
+    // LFO
+    RotaryKnob lfoRateKnob;
+    SliderAttachment lfoRateAttachment { audioProcessor.apvts, ParameterID::lfoRate.getParamID(), lfoRateKnob.slider };
+    RotaryKnob vibratoKnob;
+    SliderAttachment vibratoAttachment { audioProcessor.apvts, ParameterID::vibrato.getParamID(), vibratoKnob.slider };
+
+    // Toggles
     juce::TextButton polyModeButton;
     ButtonAttachment polyModeAttachment { audioProcessor.apvts, ParameterID::polyMode.getParamID(), polyModeButton };
+    juce::TextButton noiseTypeButton;
+    ButtonAttachment noiseTypeAttachment { audioProcessor.apvts, ParameterID::noiseType.getParamID(), noiseTypeButton };
+    juce::TextButton velocitySensitivityButton;
+    ButtonAttachment velocitySensitivityAttachment { audioProcessor.apvts, ParameterID::velocitySensitivity.getParamID(), velocitySensitivityButton };
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CppsynthAudioProcessorEditor)
 };
